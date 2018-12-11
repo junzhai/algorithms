e7ea74c4459808123982c9599e43106c6f722c3e
junzhai <j_zhai@hotmail.com>
2018-12-11 15:37:56 -0800

    233. Number of Digit One

--- src/com/leetcode/Sandbox.java
+++ src/com/leetcode/Sandbox.java
@@ -5,35 +5,26 @@ import org.junit.Assert;
 import java.util.Arrays;
 
 public class Sandbox {
-    public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int b = 0, l = people.length, ret = 0;
-        while (b < l) {
-            if (b == l - 1) {
-                ret += 1;
-                break;
-            }
-            int target = limit - people[b];
-            int p = Arrays.binarySearch(people, b, l, target);
-            if (p >= 0) {
-                while (p < l - 1 && people[p + 1] == target) {
-                    p += 1;
-                }
-
-                ret += l - p;
-                l = p;
-                b += 1;
-            } else {
-                p = -p - 1;
-                ret += l - p;
-                if (p <= b) {
-                    break;
-                }
-                ret += 1;
-                l = p - 1;
-                b += 1;
-            }
+    public int countDigitOne(int n) {
+        int d = 1;
+        while (n / d >= 10) {
+            d *= 10;
         }
+
+        int ret = 0, m = n;
+        while (d > 0) {
+            int v = n / 10 / d;
+            ret += v * d;
+            int dig = m / d;
+            if (dig > 1) {
+                ret += d;
+            } else if (dig == 1) {
+                ret += m % d + 1;
+            }
+            m %= d;
+            d /= 10;
+        }
+
         return ret;
     }
 
@@ -41,12 +32,16 @@ public class Sandbox {
         Sandbox s = new Sandbox();
         int ret;
 
-        ret = s.numRescueBoats(new int[]{4, 9, 3, 1, 1, 7, 6, 10, 10, 10, 1, 8, 8, 7, 8, 10, 7, 4, 6, 3, 6, 1, 2, 4,
-                8, 8, 4, 7, 1, 2, 10, 3, 4, 6, 3, 5, 3, 1, 2, 6, 1, 5, 4, 5, 1, 10, 5, 9, 10, 4}, 10);
-        Assert.assertEquals(29, ret);
+        ret = s.countDigitOne(100);
+        Assert.assertEquals(21, ret);
 
-        ret = s.numRescueBoats(new int[]{1, 3, 4, 3, 3, 5}, 5);
-        Assert.assertEquals(5, ret);
+        ret = s.countDigitOne(1410065408);
+        Assert.assertEquals(1737167499, ret);
 
+        ret = s.countDigitOne(9);
+        Assert.assertEquals(1, ret);
+
+        ret = s.countDigitOne(13);
+        Assert.assertEquals(6, ret);
     }
 }
