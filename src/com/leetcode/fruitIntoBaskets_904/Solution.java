c6368e0673b10327175c836db62869eb4337c4e0
junzhai <j_zhai@hotmail.com>
2019-01-27 16:04:58 -0800

    904. Fruit Into Baskets

---
+++ src/com/leetcode/fruitIntoBaskets_904/Solution.java
@@ -0,0 +1,44 @@
+package com.leetcode.fruitIntoBaskets_904;
+
+import com.pattern.SlidingWindow;
+
+import static org.junit.Assert.assertEquals;
+
+@SlidingWindow
+public class Solution {
+    public int totalFruit(int[] tree) {
+        int ret = 0;
+        for (int i = 0, b = 0, t1 = -1, t2 = -1, p1 = -1, p2 = -1; i < tree.length; i++) {
+            if (t1 == -1) {
+                t1 = tree[i];
+                p1 = i;
+            } else if (t1 == tree[i]) {
+                p1 = i;
+            } else if (t2 == -1) {
+                t2 = tree[i];
+                p2 = i;
+            } else if (t2 == tree[i]) {
+                p2 = i;
+            } else if (p1 < p2) {
+                b = p1 + 1;
+                t1 = t2;
+                p1 = p2;
+                t2 = tree[i];
+                p2 = i;
+            } else {
+                b = p2 + 1;
+                t2 = tree[i];
+                p2 = i;
+            }
+            ret = Math.max(ret, i - b + 1);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int ret;
+        ret = s.totalFruit(new int[]{0, 1, 2, 2});
+        assertEquals(3, ret);
+    }
+}
---
+++ src/com/pattern/SlidingWindow.java
@@ -0,0 +1,17 @@
+package com.pattern;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({TYPE})
+public @interface SlidingWindow {
+    String desc() default "滑动窗口，多种方法计算滑动窗口的变化。";
+
+    String[] problems() default {
+            "904. Fruit Into Baskets",
+    };
+}
