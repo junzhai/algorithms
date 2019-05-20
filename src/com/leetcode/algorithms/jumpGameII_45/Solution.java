3118325a9a33d7c71be524970700916be4db4fcc
junzhai <j_zhai@hotmail.com>
2019-05-20 14:11:11 -0700

    45. Jump Game II

---
+++ src/com/leetcode/algorithms/jumpGameII_45/Solution.java
@@ -0,0 +1,27 @@
+package com.leetcode.algorithms.jumpGameII_45;
+
+import com.leetcode.algorithms.pattern.DP;
+import org.junit.Assert;
+
+@DP
+public class Solution {
+    public int jump(int[] nums) {
+        int len = nums.length, jump = 0;
+        for (int i = 0, upper = 0, newUpper = 0; newUpper < len - 1; i++) {
+            if (i > upper) {
+                upper = newUpper;
+                newUpper = 0;
+                jump += 1;
+            }
+            newUpper = Math.max(newUpper, i + nums[i]);
+        }
+        return jump + 1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int ret;
+        ret = s.jump(new int[]{2, 3, 1, 1, 4});
+        Assert.assertEquals(2, ret);
+    }
+}
--- src/com/leetcode/algorithms/pattern/DP.java
+++ src/com/leetcode/algorithms/pattern/DP.java
@@ -36,6 +36,7 @@ public @interface DP {
             "983. Minimum Cost For Tickets",
             "808. Soup Servings",
             "446. Arithmetic Slices II - Subsequence",
-            "718. Maximum Length of Repeated Subarray"
+            "718. Maximum Length of Repeated Subarray",
+            "45. Jump Game II"
     };
 }
