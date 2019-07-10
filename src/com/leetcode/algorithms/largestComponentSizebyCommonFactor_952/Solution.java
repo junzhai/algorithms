960d8febefda3dde22a7f77b43ac1fa17ca3d27f
junzhai <j_zhai@hotmail.com>
2019-07-10 16:00:07 -0700

    952. Largest Component Size by CommonFactor

---
+++ src/com/leetcode/algorithms/largestComponentSizebyCommonFactor_952/Solution.java
@@ -0,0 +1,76 @@
+package com.leetcode.algorithms.largestComponentSizebyCommonFactor_952;
+
+import com.leetcode.algorithms.common.ArrayUtil;
+import org.junit.Assert;
+
+public class Solution {
+    public int largestComponentSize(int[] A) {
+        int[] parent = new int[100000], count = new int[100000];
+        int ret = 0;
+        for (int a : A) {
+            int f = 0;
+            if (a % 2 == 0) {
+                while (a % 2 == 0) {
+                    a /= 2;
+                }
+                f = 2;
+            }
+
+            for (int i = 3; i <= Math.sqrt(a); i += 2) {
+                if (a % i == 0) {
+                    while (a % i == 0) {
+                        a /= i;
+                    }
+                    int cf = i;
+                    while (parent[cf] != 0) {
+                        cf = parent[cf];
+                    }
+
+                    if (f == 0) {
+                        f = cf;
+                    } else if (f < cf) {
+                        parent[cf] = f;
+                        count[f] += count[cf];
+                        count[cf] = 0;
+                    } else if (f > cf) {
+                        parent[f] = cf;
+                        count[cf] += count[f];
+                        count[f] = 0;
+                        f = cf;
+                    }
+                }
+            }
+
+            if (a > 1) {
+                int cf = a;
+                while (parent[cf] != 0) {
+                    cf = parent[cf];
+                }
+
+                if (f == 0) {
+                    f = cf;
+                } else if (f < cf) {
+                    parent[cf] = f;
+                    count[f] += count[cf];
+                    count[cf] = 0;
+                } else if (f > cf) {
+                    parent[f] = cf;
+                    count[cf] += count[f];
+                    count[f] = 0;
+                    f = cf;
+                }
+            }
+
+            count[f] += 1;
+            ret = Math.max(ret, count[f]);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = ArrayUtil.readArray("src/com/leetcode/algorithms/largestComponentSizebyCommonFactor_952/case1");
+        int ret = s.largestComponentSize(input);
+        Assert.assertEquals(4878, ret);
+    }
+}
