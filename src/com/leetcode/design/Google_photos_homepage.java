038f664f372bd9d5ec681264c1a4585814d973a1
junzhai <j_zhai@hotmail.com>
2019-10-18 17:11:08 -0700

    instagram_story_feature

---
+++ src/com/leetcode/design/Google_photos_homepage.java
@@ -0,0 +1,11 @@
+package com.leetcode.design;
+
+public class Google_photos_homepage {
+    String requirement =
+            "Design a system like Google photos homepage that can show recent photos of a user along "
+                    + "with the shared photos. Interest areas -"
+                    + "1) data store & entities involved"
+                    + "2) system apis"
+                    + "3) how recent photos can be shown (order/ranking algorithm) with scrolling "
+                    + "feature in mind";
+}
---
+++ src/com/leetcode/design/instagram_story_feature.java
@@ -0,0 +1,9 @@
+package com.leetcode.design;
+
+public class instagram_story_feature {
+    String requirement =
+            "This is a design question, not sure if I can post it here. But I would love to have discussion "
+                    + "on this as this sounds an interesting design question given the feature that stories "
+                    + "only need to be visible for 24 hours, which makes it different from desigining "
+                    + "twitter/instagram etc.";
+}
