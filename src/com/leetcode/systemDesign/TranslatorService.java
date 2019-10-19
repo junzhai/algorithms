1cd9f17041395ed7a6284a0ec09178d8adb13b04
junzhai <j_zhai@hotmail.com>
2019-10-19 13:36:33 -0700

    TranslatorService

---
+++ src/com/leetcode/systemDesign/TranslatorService.java
@@ -0,0 +1,9 @@
+package com.leetcode.systemDesign;
+
+public class TranslatorService {
+    String requirement =
+            "Design a translator service to translate a message/post to multiple language on facebook";
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/"
+                    + "386322/Design-a-translator-service-for-facebook";
+}
