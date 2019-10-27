41b595cfffc82d479ebab35ac7881991bcdc924b
junzhai <j_zhai@hotmail.com>
2019-10-27 16:56:14 -0700

    Leadership Board System

---
+++ src/com/leetcode/systemDesign/LeadershipBoardSystem.java
@@ -0,0 +1,17 @@
+package com.leetcode.systemDesign;
+
+public class LeadershipBoardSystem {
+    String[] requirement = new String[]{
+            "1) How would you implement ranking in real time?",
+            "2) Will the leaderboard be in real time? Or are there any limitations to how often the leaderboard can "
+                    + "be calculated?",
+            "3) What happens to the leaderboard if the players are currently playing? How can we provide a "
+                    + "leaderboard that is processing streaming data in real time?",
+            "4) What algorithms can we use to calculate top 15 players for 1 day, 3 days, 7 days and 1 month?",
+            "5) What about leaderboards for regions/districts/games/friend circles/etc?"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/409736/Facebook-or-System-Design-or-Hacker"
+                    + "-Rank-LeetCode-Contest-Leadership-Board-System";
+}
