08a086e739f7f39e1f0f7f6f0a5c61b658aa8f4a
junzhai <j_zhai@hotmail.com>
2019-10-29 15:52:16 -0700

    Task Executor as Saas

---
+++ src/com/leetcode/systemDesign/TaskExecutorAsSaas.java
@@ -0,0 +1,12 @@
+package com.leetcode.systemDesign;
+
+public class TaskExecutorAsSaas {
+    String[] requirement = new String[]{
+            "You are given a VM and tomcat server. Design a optimized Task executor system. Feel free to add or " +
+                    "change anything. Question was completely open ended and interviewer were mostly looking for what" +
+                    " different design solution(s)."
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/385363/Design-Task-Executor-as-Saas";
+}
