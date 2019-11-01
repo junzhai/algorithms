e13dda364d55212cf1d972ab569df3e435a7d9f1
junzhai <j_zhai@hotmail.com>
2019-11-01 14:05:10 -0700

    Health Score App

---
+++ src/com/leetcode/systemDesign/HealthScoreApp.java
@@ -0,0 +1,12 @@
+package com.leetcode.systemDesign;
+
+public class HealthScoreApp {
+    String[] requirement = new String[]{
+            "People can compete with each other. Display chart which shows score of top hundres people.",
+            "Score can be calculated based on steps, exercise, calories burned."
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/366754/Amazon-or-System-Design-for-health" +
+                    "-score-app";
+}
