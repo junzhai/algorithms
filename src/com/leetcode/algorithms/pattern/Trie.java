91e74db58bf4f96db04120267e0e6c1175527267
junzhai <j_zhai@hotmail.com>
2019-11-01 18:31:12 -0700

    792. Number of Matching Subsequences

--- src/com/leetcode/algorithms/pattern/Trie.java
+++ src/com/leetcode/algorithms/pattern/Trie.java
@@ -16,6 +16,7 @@ public @interface Trie {
             "745. Prefix and Suffix Search （插入变种字串，'二维Trie树'）",
             "966. Vowel Spellchecker",
             "1032. Stream of Characters",
-            "164. Maximum Gap（压缩节点）"
+            "164. Maximum Gap（压缩节点）",
+            "792. Number of Matching Subsequences（Trie树构建过程有技巧）"
     };
 }
