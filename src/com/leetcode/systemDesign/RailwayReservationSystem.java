ff6f631beac54ade4bd128399689bb8a6fcafaf0
junzhai <j_zhai@hotmail.com>
2019-11-04 16:16:41 -0800

    Railway Reservation System

---
+++ src/com/leetcode/systemDesign/RailwayReservationSystem.java
@@ -0,0 +1,9 @@
+package com.leetcode.systemDesign;
+
+public class RailwayReservationSystem {
+    String[] requirement = new String[]{
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/364965/Railway-Reservation-System";
+}
