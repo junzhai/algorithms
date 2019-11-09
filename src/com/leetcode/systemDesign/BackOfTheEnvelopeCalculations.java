08b3432eeac1bad9619efc7f15c4c5896f040cf9
junzhai <j_zhai@hotmail.com>
2019-11-09 13:14:05 -0800

    Back-of-the-envelope Calculations

---
+++ src/com/leetcode/systemDesign/BackOfTheEnvelopeCalculations.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class BackOfTheEnvelopeCalculations {
+    String[] requirement = new String[]{
+            "Experience with back-of-the-envelope calculations"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/357656/Experience-with-back-of-the" +
+                    "-envelope-calculations";
+}
