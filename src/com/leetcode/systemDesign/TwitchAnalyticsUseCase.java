e255605860af94a7d611a00af46d2d251ecca926
junzhai <j_zhai@hotmail.com>
2019-11-14 14:27:56 -0800

    Twitch Analytics Use Case

---
+++ src/com/leetcode/systemDesign/TwitchAnalyticsUseCase.java
@@ -0,0 +1,17 @@
+package com.leetcode.systemDesign;
+
+public class TwitchAnalyticsUseCase {
+    String[] requirement = new String[]{
+            "Given:",
+            "1) There is already defined Twitch video stream and chat box with a viewer count.",
+            "2) There is a User Service that requires User_id and provides User's Age and Gender.",
+            "The interviewer is a PM and wants to check if this feature is possible.",
+            "Question:",
+            "Need to add Analytics dashboard of all viewers based on Gender, Agde distribuution graphs below the " +
+                    "video stream."
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/421969/Twitch-or-System-Design-Onsite-or" +
+                    "-Design-Twitch-Analytics-Use-Case";
+}
