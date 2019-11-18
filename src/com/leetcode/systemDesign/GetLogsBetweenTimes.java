f21b2b6e6230694f6e835466752cb0e1e5276cb0
junzhai <j_zhai@hotmail.com>
2019-11-18 14:40:11 -0800

    Get Logs Between Times

---
+++ src/com/leetcode/systemDesign/GetLogsBetweenTimes.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class GetLogsBetweenTimes {
+    String[] requirement = new String[]{
+            "Design an API that supports the following function calls:"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/431023/Google-or-Onsite-or-Get-all-logs" +
+                    "-between-times";
+}
