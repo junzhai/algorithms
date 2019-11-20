8f7af66ee296e347b78d294284b72e2078da4956
junzhai <j_zhai@hotmail.com>
2019-11-20 13:38:32 -0800

    Nearby Friends

---
+++ src/com/leetcode/systemDesign/NearbyFriends.java
@@ -0,0 +1,10 @@
+package com.leetcode.systemDesign;
+
+public class NearbyFriends {
+    String[] requirement = new String[]{
+            "Design Nearby Friends"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/430926/Design-Nearby-Friends";
+}
