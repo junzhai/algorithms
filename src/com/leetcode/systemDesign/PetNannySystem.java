f7452abe76beef19f786a2b1908828720370d0e9
junzhai <j_zhai@hotmail.com>
2019-11-23 16:30:10 -0800

    Pet Nanny System

---
+++ src/com/leetcode/systemDesign/PetNannySystem.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class PetNannySystem {
+    String[] requirement = new String[]{
+            "Design a Service for a Pet Nanny system."
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/418381/Design-a-Service-for-Pet-Nanny" +
+                    "-System";
+}
