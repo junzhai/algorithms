914cbe1f129d32589e39ccb558e4f73b3f3b2a0d
junzhai <j_zhai@hotmail.com>
2019-11-26 16:17:23 -0800

    Coupon Voucher Management System

---
+++ src/com/leetcode/systemDesign/CouponVoucherManagementSystem.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class CouponVoucherManagementSystem {
+    String[] requirement = new String[]{
+            "Design a coupon/voucher management system | DellEMC"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/353302/Design-a-couponvoucher-management" +
+                    "-system-or-DellEMC";
+}
