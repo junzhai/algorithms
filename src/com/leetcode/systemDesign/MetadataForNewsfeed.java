ce56219e9ee3b42eea17bda5fdea87de6f82842c
junzhai <j_zhai@hotmail.com>
2019-11-27 16:15:15 -0800

    Metadata For Newsfeed

---
+++ src/com/leetcode/systemDesign/MetadataForNewsfeed.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class MetadataForNewsfeed {
+    String[] requirement = new String[]{
+            "How do you design a meta data for a news feed"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/349627/How-do-you-design-a-meta-data-for-a" +
+                    "-news-feed";
+}
