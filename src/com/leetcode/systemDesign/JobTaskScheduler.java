4378654d97fc9feb332981ce6a3da39ef1c454e5
junzhai <j_zhai@hotmail.com>
2019-11-29 16:15:01 -0800

    Job/Task Scheduler

---
+++ src/com/leetcode/systemDesign/JobTaskScheduler.java
@@ -0,0 +1,14 @@
+package com.leetcode.systemDesign;
+
+public class JobTaskScheduler {
+    String[] requirement = new String[]{
+            "Amazon | Design a Job/Task Scheduler"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/344524/Amazon-or-Design-a-JobTask-Scheduler";
+
+    String reference =
+            "https://medium.com/walmartlabs/an-approach-to-designing-distributed-fault-tolerant-horizontally-scalable" +
+                    "-event-scheduler-278c9c380637";
+}
