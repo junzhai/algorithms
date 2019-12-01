9cb3eb5a7b96f89f7014dae5605031746e0878dd
junzhai <j_zhai@hotmail.com>
2019-11-30 17:47:56 -0800

    Subscribe And Save System Like Amazon

---
+++ src/com/leetcode/systemDesign/SubscribeAndSaveSystemLikeAmazon.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class SubscribeAndSaveSystemLikeAmazon {
+    String[] requirement = new String[]{
+            "Design Subscribe and Save system like Amazon"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/342773/Design-Subscribe-and-Save-system" +
+                    "-like-Amazon";
+}
