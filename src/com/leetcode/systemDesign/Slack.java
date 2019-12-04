56fe713c09d4c9f7c4b77bba6268309ded608641
junzhai <j_zhai@hotmail.com>
2019-12-04 11:49:04 -0800

    Slack

---
+++ src/com/leetcode/systemDesign/Slack.java
@@ -0,0 +1,10 @@
+package com.leetcode.systemDesign;
+
+public class Slack {
+    String[] requirement = new String[]{
+            "System Design | Slack"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/339849/System-Design-or-Slack";
+}
