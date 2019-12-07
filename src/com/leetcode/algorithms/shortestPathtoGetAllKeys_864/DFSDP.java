fbdc729981b202a3f63a91edda8bf5831400991e
junzhai <j_zhai@hotmail.com>
2019-12-06 17:19:40 -0800

    864. Shortest Path to Get All Keys (updated)

---
+++ src/com/leetcode/algorithms/shortestPathtoGetAllKeys_864/DFSDP.java
@@ -0,0 +1,96 @@
+package com.leetcode.algorithms.shortestPathtoGetAllKeys_864;
+
+import com.leetcode.algorithms.pattern.DFS;
+import com.leetcode.algorithms.pattern.DP;
+
+@DFS
+@DP
+public class DFSDP extends Solution {
+    @Override
+    public int shortestPathAllKeys(String[] grid) {
+        int row = grid.length, col = grid[0].length();
+        char[][] g = new char[row][0];
+        int r0 = 0, c0 = 0, key = 0;
+        for (int i = 0; i < row; i++) {
+            g[i] = grid[i].toCharArray();
+            for (int j = 0; j < col; j++) {
+                char ch = g[i][j];
+                if (ch == '@') {
+                    r0 = i;
+                    c0 = j;
+                }
+                if (ch >= 'a' && ch <= 'f') {
+                    key = Math.max(key, ch - 'a');
+                }
+            }
+        }
+
+        int target = (1 << key + 1) - 1;
+        boolean[][][] visited = new boolean[row][col][target + 1];
+
+        int[][][] dp = new int[row][col][target + 1];
+        dp[r0][c0][0] = 1;
+
+        int[] ret = new int[1];
+        int max = (key + 1) * (row + col - 2) + 1;
+        ret[0] = max;
+
+        minPath(g, r0, c0, 0, 0, target, visited, dp, ret);
+        return ret[0] == max ? -1 : ret[0];
+    }
+
+    private void minPath(char[][] g, int r, int c, int k, int step, int target, boolean[][][] visited, int[][][] dp,
+                         int[] ret) {
+        if (step >= ret[0]) {
+            return;
+        }
+
+        if (visited[r][c][k]) {
+            return;
+        }
+
+        char ch = g[r][c];
+        if (ch == '#') {
+            return;
+        }
+
+        if (ch >= 'a' && ch <= 'f') {
+            k |= (1 << ch - 'a');
+        }
+
+        if (ch >= 'A' && ch <= 'F' && ((1 << ch - 'A') & k) == 0) {
+            return;
+        }
+
+        if (dp[r][c][k] > 0 || ch == '@' && k == 0) {
+            if (step >= dp[r][c][k]) {
+                return;
+            }
+        }
+
+        dp[r][c][k] = step;
+        if (k == target) {
+            ret[0] = Math.min(ret[0], step);
+            return;
+        }
+
+        int row = g.length, col = g[0].length;
+        visited[r][c][k] = true;
+        if (c + 1 < col) {
+            minPath(g, r, c + 1, k, step + 1, target, visited, dp, ret);
+        }
+
+        if (r + 1 < row) {
+            minPath(g, r + 1, c, k, step + 1, target, visited, dp, ret);
+        }
+
+        if (c > 0) {
+            minPath(g, r, c - 1, k, step + 1, target, visited, dp, ret);
+        }
+
+        if (r > 0) {
+            minPath(g, r - 1, c, k, step + 1, target, visited, dp, ret);
+        }
+        visited[r][c][k] = false;
+    }
+}
--- src/com/leetcode/algorithms/shortestPathtoGetAllKeys_864/Solution.java
+++ src/com/leetcode/algorithms/shortestPathtoGetAllKeys_864/Solution.java
@@ -6,69 +6,103 @@ abstract public class Solution {
     abstract public int shortestPathAllKeys(String[] grid);
 
     public static void main(String[] args) {
-//        Solution s = new BruteForce();
-//        Solution s = new UseDP();
-        Solution s = new UseBFS();
+        Solution[] solutions = new Solution[]{
+                new BruteForce(),
+                new DFSDP(),
+                new UseDP(),
+                new UseBFS()
+        };
         int ret;
 
-//        ret = s.shortestPathAllKeys(new String[]{
-//                "@abcdeABCDEFf"});
-//        Assert.assertEquals(-1, ret);
+        for (Solution s : solutions) {
+            ret = s.shortestPathAllKeys(new String[]{"@abcdeABCDEFf"});
+            Assert.assertEquals(-1, ret);
 
-        ret = s.shortestPathAllKeys(new String[]{
-                "@...a",
-                ".###A",
-                "b.BCc"});
-        Assert.assertEquals(10, ret);
+            ret = s.shortestPathAllKeys(new String[]{
+                    "@...a",
+                    ".###A",
+                    "b.BCc"});
+            Assert.assertEquals(10, ret);
 
-        ret = s.shortestPathAllKeys(new String[]{
-                "@Aa"});
-        Assert.assertEquals(-1, ret);
+            ret = s.shortestPathAllKeys(new String[]{"@Aa"});
+            Assert.assertEquals(-1, ret);
 
+            ret = s.shortestPathAllKeys(new String[]{
+                    "#..#.#.#..#.#.#.....#......#..",
+                    ".#.......#....#A.....#.#......",
+                    "#....#.....#.........#........",
+                    "...#.#.........#..@....#....#.",
+                    ".#.#.##...#.........##....#..#",
+                    "..........#..#..###....##..#.#",
+                    ".......#......#...#...#.....c#",
+                    ".#...#.##......#...#.###...#..",
+                    "..........##...#.......#......",
+                    "#...#.........a#....#.#.##....",
+                    "..#..#...#...#..#....#.....##.",
+                    "..........#...#.##............",
+                    "...#....#..#.........#..D.....",
+                    "....#E.#....##................",
+                    "...........##.#.......#.#....#",
+                    "...#..#...#.#............#e...",
+                    "..#####....#.#...........##..#",
+                    "##......##......#.#...#..#.#..",
+                    ".#F.......#..##.......#....#..",
+                    "............#....#..#..#...#..",
+                    ".............#...#f...#..##...",
+                    "....#..#...##.........#..#..#.",
+                    ".....#.....##.###..##.#......#",
+                    ".#..#.#...#.....#........###..",
+                    ".....#.#...#...#.....#.....#..",
+                    "##.....#....B.....#..#b.......",
+                    ".####....##..#.##..d.#......#.",
+                    "..#.....#....##........##...##",
+                    "...#...#...C..#..#....#.......",
+                    "#.....##.....#.#......#......."});
+            Assert.assertEquals(70, ret);
 
-        ret = s.shortestPathAllKeys(new String[]{
-                "#..#.#.#..#.#.#.....#......#..",
-                ".#.......#....#A.....#.#......",
-                "#....#.....#.........#........",
-                "...#.#.........#..@....#....#.",
-                ".#.#.##...#.........##....#..#",
-                "..........#..#..###....##..#.#",
-                ".......#......#...#...#.....c#",
-                ".#...#.##......#...#.###...#..",
-                "..........##...#.......#......",
-                "#...#.........a#....#.#.##....",
-                "..#..#...#...#..#....#.....##.",
-                "..........#...#.##............",
-                "...#....#..#.........#..D.....",
-                "....#E.#....##................",
-                "...........##.#.......#.#....#",
-                "...#..#...#.#............#e...",
-                "..#####....#.#...........##..#",
-                "##......##......#.#...#..#.#..",
-                ".#F.......#..##.......#....#..",
-                "............#....#..#..#...#..",
-                ".............#...#f...#..##...",
-                "....#..#...##.........#..#..#.",
-                ".....#.....##.###..##.#......#",
-                ".#..#.#...#.....#........###..",
-                ".....#.#...#...#.....#.....#..",
-                "##.....#....B.....#..#b.......",
-                ".####....##..#.##..d.#......#.",
-                "..#.....#....##........##...##",
-                "...#...#...C..#..#....#.......",
-                "#.....##.....#.#......#......."});
-        Assert.assertEquals(70, ret);
+            ret = s.shortestPathAllKeys(new String[]{
+                    "@..aA",
+                    "..B#.",
+                    "....b"});
+            Assert.assertEquals(6, ret);
 
-        ret = s.shortestPathAllKeys(new String[]{
-                "@..aA",
-                "..B#.",
-                "....b"});
-        Assert.assertEquals(6, ret);
+            ret = s.shortestPathAllKeys(new String[]{
+                    "@.a.#",
+                    "###.#",
+                    "b.A.B"});
+            Assert.assertEquals(8, ret);
 
-        ret = s.shortestPathAllKeys(new String[]{
-                "@.a.#",
-                "###.#",
-                "b.A.B"});
-        Assert.assertEquals(8, ret);
+            ret = s.shortestPathAllKeys(new String[]{
+                    "###.D.B.F.",
+                    "..#b......",
+                    "##....#..#",
+                    "....@..#..",
+                    "#d.AE.##c#",
+                    ".....##.f.",
+                    "#........C",
+                    "..a#.#....",
+                    "#....#e..#",
+                    "........##"});
+            Assert.assertEquals(25, ret);
+
+            ret = s.shortestPathAllKeys(new String[]{"@Aa"});
+            Assert.assertEquals(-1, ret);
+
+            ret = s.shortestPathAllKeys(new String[]{
+                    "...#.",
+                    "a..@.",
+                    "#..#.",
+                    "b.#B.",
+                    ".##.A"});
+            Assert.assertEquals(7, ret);
+
+            ret = s.shortestPathAllKeys(new String[]{
+                    "Dd#b@",
+                    ".fE.e",
+                    "##.B.",
+                    "#.cA.",
+                    "aF.#C"});
+            Assert.assertEquals(14, ret);
+        }
     }
 }
