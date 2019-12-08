1df1d40188b23055c2642296bc3e9f93798c9a92
junzhai <j_zhai@hotmail.com>
2019-12-08 14:12:09 -0800

    GoFundMe

---
+++ src/com/leetcode/systemDesign/GoFundMe.java
@@ -0,0 +1,12 @@
+package com.leetcode.systemDesign;
+
+public class GoFundMe {
+    String[] requirement = new String[]{
+            "GoFundMe"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/336089/System-Design-or-GoFundMe";
+
+    String reference = "https://en.wikipedia.org/wiki/Payment_gateway";
+}
