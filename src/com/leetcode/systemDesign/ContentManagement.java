b388dcaa95411af48184e8d72d3acf5d5248bfb9
junzhai <j_zhai@hotmail.com>
2019-12-10 15:41:54 -0800

    Content Management

---
+++ src/com/leetcode/systemDesign/ContentManagement.java
@@ -0,0 +1,10 @@
+package com.leetcode.systemDesign;
+
+public class ContentManagement {
+    String[] requirement = new String[]{
+            "Content Management System Design"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/322448/Content-Management-System-Design";
+}
