c552417581263c6d0158e66c46a9b97fe40d2c00
junzhai <j_zhai@hotmail.com>
2019-12-12 14:02:18 -0800

    Scalable Shipping Fulfillment Center

---
+++ src/com/leetcode/systemDesign/ScalableShippingFulfillmentCenter.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class ScalableShippingFulfillmentCenter {
+    String[] requirement = new String[]{
+            "Design: Scalable Shipping Fulfillment Center"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/320719/Design%3A-Scalable-Shipping" +
+                    "-Fulfillment-Center";
+}
