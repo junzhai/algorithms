04b0e59c0fda433c335db5f42239c272b67800cb
junzhai <j_zhai@hotmail.com>
2019-12-14 11:27:44 -0800

    Seat Reservation Application

---
+++ src/com/leetcode/systemDesign/SeatReservationApplication.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class SeatReservationApplication {
+    String[] requirement = new String[]{
+            "Seat reservation application like Ticket Master or BookMyShow"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/315763/System-Design-or-Seat-reservation" +
+                    "-application-like-Ticket-Master-or-BookMyShow";
+}
