58be78988f059914e5929a13c8899f1b418b3164
junzhai <j_zhai@hotmail.com>
2019-12-16 11:50:46 -0800

    Tracking Bad Request

---
+++ src/com/leetcode/systemDesign/TrackingBadRequest.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class TrackingBadRequest {
+    String[] requirement = new String[]{
+            "Design a system capable of tracking bad request down quickly"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/313117/Design-a-system-capable-of-tracking" +
+                    "-bad-request-down-quickly";
+}
