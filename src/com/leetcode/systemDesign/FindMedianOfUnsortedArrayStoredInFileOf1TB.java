338d4d67cbb12c0d2ea72f96a1755b2dc0445cf8
junzhai <j_zhai@hotmail.com>
2019-12-22 16:34:09 -0800

    Find Median Of Unsorted Array Stored In File Of 1 TB

---
+++ src/com/leetcode/systemDesign/FindMedianOfUnsortedArrayStoredInFileOf1TB.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class FindMedianOfUnsortedArrayStoredInFileOf1TB {
+    String[] requirement = new String[]{
+            "Find out median of an unsorted array stored in file size of 1 TB"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/456340/Find-out-median-of-an-unsorted" +
+                    "-array-stored-in-file-size-of-1-TB.";
+}
