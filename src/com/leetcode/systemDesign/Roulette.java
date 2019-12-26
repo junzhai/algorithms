b02d11ed8386ab8489b6a72f683f0e344bfa3636
junzhai <j_zhai@hotmail.com>
2019-12-26 15:19:39 -0800

    Roulette

---
+++ src/com/leetcode/systemDesign/Roulette.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class Roulette {
+    String[] requirement = new String[]{
+            "Roulette"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/object-oriented-design/408866/Roulette" +
+                    "(Object-Oriented-Desgin)-Any-ideas-how-to-do-this";
+}
