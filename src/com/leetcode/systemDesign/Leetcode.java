61c58efb407b08845bd65d32b0eaf60570e5cf6d
junzhai <j_zhai@hotmail.com>
2019-12-25 16:30:24 -0800

    Leetcode

---
+++ src/com/leetcode/systemDesign/Leetcode.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class Leetcode {
+    String[] requirement = new String[]{
+            "Programming contest platform like LeetCode"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/308452/System-Design-or-Programming" +
+                    "-contest-platform-like-LeetCode";
+}
