6810d217c77612bf242bdfe52d49caf1b306bec5
junzhai <j_zhai@hotmail.com>
2019-12-30 12:47:10 -0800

    P2P File Sharing

---
+++ src/com/leetcode/systemDesign/P2PFileSharing.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class P2PFileSharing {
+    String[] requirement = new String[]{
+            "Design a P2P file sharing application like BitTorrent"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/464997/Design-a-P2P-file-sharing" +
+                    "-application-like-BitTorrent";
+}
