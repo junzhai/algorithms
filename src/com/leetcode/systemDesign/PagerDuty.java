6c1ba52c0cf0e8224530e7a698d0075efd6853b2
junzhai <j_zhai@hotmail.com>
2020-01-07 11:38:37 -0800

    PagerDuty

---
+++ src/com/leetcode/systemDesign/PagerDuty.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class PagerDuty {
+    String[] requirement = new String[]{
+            "Design a pager service like Pagerduty"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/472784/Design-a-pager-service-like" +
+                    "-Pagerduty";
+}
