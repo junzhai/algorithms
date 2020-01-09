5c142b3b6909033d0a3b4b2041214751bc97dac2
junzhai <j_zhai@hotmail.com>
2020-01-09 10:25:21 -0800

    Pickup Delivery System For Groceries

---
+++ src/com/leetcode/systemDesign/PickupDeliverySystemForGroceries.java
@@ -0,0 +1,11 @@
+package com.leetcode.systemDesign;
+
+public class PickupDeliverySystemForGroceries {
+    String[] requirement = new String[]{
+            "System Design Pickup Delivery System For Groceries"
+    };
+
+    String discussion =
+            "https://leetcode.com/discuss/interview-question/system-design/467655/Amazon-Onsite-or-System-Design" +
+                    "-Pickup-Delivery-System-For-Groceries";
+}
