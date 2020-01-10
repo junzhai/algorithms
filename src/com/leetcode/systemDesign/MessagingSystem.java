b41e124de22e07837c17f1bd05828ddcf0d66384
junzhai <j_zhai@hotmail.com>
2020-01-10 12:37:48 -0800

    Messaging System

---
+++ src/com/leetcode/systemDesign/MessagingSystem.java
@@ -0,0 +1,20 @@
+package com.leetcode.systemDesign;
+
+public class MessagingSystem {
+    String[] examples = new String[]{
+            "Slack",
+            "What's App",
+            "WeChat"
+    };
+
+    String[] requirement = new String[]{
+            "System Design | Slack",
+            "Object Oriented Design for WhatsApp/WeChat (Chat app)"
+    };
+
+    String[] discussions = new String[]{
+            "https://leetcode.com/discuss/interview-question/system-design/339849/System-Design-or-Slack",
+            "https://leetcode.com/discuss/interview-question/object-oriented-design/385365/Object-Oriented-Design-for" +
+                    "-WhatsAppWeChat-(Chat-app)"
+    };
+}
