fb67292ef9f2543ef7dfae010af5d2ffd1406937
junzhai <j_zhai@hotmail.com>
2020-02-23 11:50:24 -0800

    395. Longest Substring with At Least K Repeating Characters

--- src/com/leetcode/algorithms/pattern/DivideAndConquer.java
+++ src/com/leetcode/algorithms/pattern/DivideAndConquer.java
@@ -16,6 +16,7 @@ public @interface DivideAndConquer {
             "327. Count of Range Sum",
             "1044. Longest Duplicate Substring",
             "493. Reverse Pairs",
-            "152. Maximum Product Subarray"
+            "152. Maximum Product Subarray",
+            "395. Longest Substring with At Least K Repeating Characters"
     };
 }
