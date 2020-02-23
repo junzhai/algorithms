6adc38181c58cfdf07dc23d83835cc8a0fa03c38
junzhai <j_zhai@hotmail.com>
2020-02-23 11:51:33 -0800

    minor fix

--- src/com/leetcode/algorithms/pattern/WinnerLoserTournamentTree.java
+++ src/com/leetcode/algorithms/pattern/WinnerLoserTournamentTree.java
@@ -8,7 +8,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
 @Target({TYPE})
-public @interface WinnerTree {
+public @interface WinnerLoserTournamentTree {
     String desc() default "";
 
     String[] problems() default {
