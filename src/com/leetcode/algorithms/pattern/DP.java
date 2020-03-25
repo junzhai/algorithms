383cfbb839e74607e857f442619bb7c064215597
junzhai <j_zhai@hotmail.com>
2020-03-24 18:48:35 -0700

    1314. Matrix Block Sum

--- src/com/leetcode/algorithms/pattern/DP.java
+++ src/com/leetcode/algorithms/pattern/DP.java
@@ -86,6 +86,7 @@ public @interface DP {
             "1079. Letter Tile Possibilities",
             "279. Perfect Squares",
             "1235. Maximum Profit in Job Scheduling",
-            "1004. Max Consecutive Ones III"
+            "1004. Max Consecutive Ones III",
+            "1314. Matrix Block Sum"
     };
 }
