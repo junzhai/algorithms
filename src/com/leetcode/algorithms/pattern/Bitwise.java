495f37e959c89d0bac2328d3e15b15d8b27d64e4
Jun Zhai <j_zhai@hotmail.com>
2020-10-05 15:42:10 -0700

    1009. Complement of Base 10 Integer

--- src/com/leetcode/algorithms/pattern/Bitwise.java
+++ src/com/leetcode/algorithms/pattern/Bitwise.java
@@ -9,12 +9,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({TYPE})
 public @interface Bitwise {
-    String desc() default "bitwise operation";
+    String desc() default "bitwise operation, Java use 2's complement to represent negative number.";
 
     String[] problems() default {
             "854. K-Similar Strings",
             "1177. Can Make Palindrome from Substring",
             "433. Minimum Genetic Mutation",
-            "421. Maximum XOR of Two Numbers in an Array"
+            "421. Maximum XOR of Two Numbers in an Array",
+            "1009. Complement of Base 10 Integer"
     };
 }
