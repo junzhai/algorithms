309ba6aa5c49c2908d5639dcd3ea7a5bfe2c96a3
Jun Zhai <j_zhai@hotmail.com>
2020-12-02 18:47:33 -0800

    382. Linked List Random Node

---
+++ java/src/com/leetcode/algorithms/pattern/Probability.java
@@ -0,0 +1,17 @@
+package com.leetcode.algorithms.pattern;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({TYPE})
+public @interface Probability {
+    String desc() default "各种几率计算";
+
+    String[] problems() default {
+            "382. Linked List Random Node"
+    };
+}
