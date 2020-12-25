b53ad641abf023c3b308001d0befe796508855a6
Jun Zhai <j_zhai@hotmail.com>
2020-12-25 10:42:07 -0800

    131. Palindrome Partitioning

--- java/src/com/leetcode/algorithms/pattern/DP.java
+++ java/src/com/leetcode/algorithms/pattern/DP.java
@@ -95,6 +95,7 @@ public @interface DP {
             "518. Coin Change 2",
             "140. Word Break II （两种方法，recursive/iterative, recursive不需历遍所有状态，可能会比iterative方法快）",
             "713. Subarray Product Less Than K",
-            "213. House Robber II"
+            "213. House Robber II",
+            "131. Palindrome Partitioning"
     };
 }
