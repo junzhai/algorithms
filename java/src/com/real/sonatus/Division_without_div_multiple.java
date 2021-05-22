13784fbf9815eb48c4ee12c8770334a221e8d296
junzhai <j_zhai@hotmail.com>
2021-05-21 17:50:26 -0700

    Sonatus interview question

---
+++ java/src/com/real/sonatus/Division_without_div_multiple.java
@@ -0,0 +1,23 @@
+package com.real.sonatus;
+
+public class Division_without_div_multiple {
+    // O(log x/y)
+    int solution(int x, int y) {
+        int[][] ys = new int[100][2];
+        int t = y, c = 1, len = 0;
+        while (t < x) {
+            ys[len][0] = t;
+            ys[len++][1] = c;
+            t += t;
+            c += c;
+        }
+        int ret = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            while (ys[i][0] <= x) {
+                ret += ys[i][1];
+                x -= ys[i][0];
+            }
+        }
+        return ret;
+    }
+}
