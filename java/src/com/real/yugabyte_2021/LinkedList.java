5c8ca0868d7e35b4f34dfd91a7116b1cbca83981
junzhai <j_zhai@hotmail.com>
2021-06-22 11:57:01 -0700

    yugabyte

---
+++ java/src/com/real/yugabyte_2021/LinkedList.java
@@ -0,0 +1,150 @@
+package com.real.yugabyte_2021;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Circular singly list in ascending order
+ */
+public class LinkedList {
+    public static void main(String[] args) {
+        Solution_1 s = new Solution_1();
+        s.insert(2);
+        s.insert(1);
+        s.insert(5);
+        s.insert(10);
+        s.insert(0);
+        s.insert(4);
+        s.print();
+
+        Solution_2 s2 = new Solution_2();
+        s2.insert(2);
+        s2.insert(1);
+        s2.insert(5);
+        s2.insert(10);
+        s2.insert(0);
+        s2.insert(4);
+        s2.print();
+    }
+
+    private static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private static class Solution_1 {
+        private int sum = 0;
+        private Node head = null, tail = null;
+
+        public void insert(int x) {
+            Node n = new Node(x);
+            if (head == null) {
+                head = n;
+                tail = n;
+                n.next = n;
+            } else {
+                Node p = head, pre = null;
+                while (p != tail && p.val < x) {
+                    pre = p;
+                    p = p.next;
+                }
+
+                if (p.val >= x) {
+                    if (pre == null) {
+                        n.next = head;
+                        head = n;
+                        tail.next = n;
+                    } else {
+                        n.next = pre.next;
+                        pre.next = n;
+                    }
+                } else {
+                    p.next = n;
+                    n.next = head;
+                    tail = n;
+                }
+            }
+            sum += x;
+        }
+
+        public int getSum() {
+            return sum;
+        }
+
+        public void print() {
+            Node p = head;
+            while (p != tail) {
+                System.out.print(p.val + ", ");
+                p = p.next;
+            }
+            System.out.println(p.val);
+        }
+    }
+
+    /**
+     * unnecessary
+     */
+    private static class Solution_2 {
+        private int sum = 0;
+        private Node head = null, tail = null;
+        private List<Node> arr = new ArrayList<>();
+
+        private Comparator<Node> comp = new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.val - o2.val;
+            }
+        };
+
+        public void insert(int x) {
+            Node n = new Node(x);
+            if (head == null) {
+                head = n;
+                tail = n;
+                n.next = n;
+                arr.add(n);
+            } else {
+                int i = Collections.binarySearch(arr, n, comp);
+                if (i < 0) {
+                    i = -i - 1;
+                }
+
+                arr.add(i, n);
+
+                if (i == 0) {
+                    head = n;
+                    tail.next = n;
+                    n.next = arr.get(i + 1);
+                } else if (i == arr.size() - 1) {
+                    arr.get(i - 1).next = n;
+                    n.next = head;
+                    tail = n;
+                } else {
+                    arr.get(i - 1).next = n;
+                    n.next = arr.get(i + 1);
+                }
+            }
+            sum += x;
+        }
+
+        public int getSum() {
+            return sum;
+        }
+
+        public void print() {
+            Node p = head;
+            while (p != tail) {
+                System.out.print(p.val + ", ");
+                p = p.next;
+            }
+            System.out.println(p.val);
+
+        }
+    }
+}
---
+++ java/src/com/real/yugabyte_2021/PrintFolders.java
@@ -0,0 +1,49 @@
+package com.real.yugabyte_2021;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrintFolders {
+    public static void main(String[] args) {
+        PrintFolders s = new PrintFolders();
+        s.solution(new String[]{"a/b/c/d", "a/b/e/d", "test/ss", "test/ss/er", "test/ss/dds"});
+    }
+
+    private static class Trie {
+        String path;
+        Map<String, Trie> children = new HashMap<>();
+
+        Trie(String path) {
+            this.path = path;
+        }
+    }
+
+    public void solution(String[] folders) {
+        Trie root = new Trie("");
+        for (String folder : folders) {
+            String[] arr = folder.split("/");
+            Trie p = root;
+            for (String sub : arr) {
+                if (!p.children.containsKey(sub)) {
+                    p.children.put(sub, new Trie(sub));
+                }
+                p = p.children.get(sub);
+            }
+        }
+
+        dfsPrint(root, -1);
+    }
+
+    private void dfsPrint(Trie p, int h) {
+        if (!p.path.isEmpty()) {
+            for (int i = 0; i < h; i++) {
+                System.out.print("\t");
+            }
+            System.out.println(p.path);
+        }
+
+        for (Trie child : p.children.values()) {
+            dfsPrint(child, h + 1);
+        }
+    }
+}
