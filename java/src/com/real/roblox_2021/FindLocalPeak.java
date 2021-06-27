3877e33a6624116e42937bbb03b558b9c96e52e0
junzhai <j_zhai@hotmail.com>
2021-06-27 13:36:23 -0700

    Roblox

---
+++ java/src/com/real/roblox_2021/FindLocalPeak.java
@@ -0,0 +1,47 @@
+package com.real.roblox_2021;
+
+public class FindLocalPeak {
+    public static void main(String[] args) {
+        FindLocalPeak s = new FindLocalPeak();
+        s.solution_1(new int[]{2, 3, 7, 1, 3, 2, 1, 4, 9});
+        s.solution_1(new int[]{9, 2, 3, 7, 1, 3, 2, 1, 4, 9});
+        s.solution_2(new int[]{9, 2, 3, 7, 7, 1, 3, 3, 3, 3, 2, 1, 1, 4, 9, 9, 10, 21, 21, 3});
+    }
+
+    // Distinct values
+    public void solution_1(int[] arr) {
+        boolean up = false;
+        for (int i = 1; i < arr.length; i++) {
+            if (up) {
+                if (arr[i] < arr[i - 1]) {
+                    System.out.println((i - 1) + ", " + arr[i - 1]);
+                    up = false;
+                }
+            } else {
+                if (arr[i] > arr[i - 1]) {
+                    up = true;
+                }
+            }
+        }
+    }
+
+    // Consider duplicated values
+    public void solution_2(int[] arr) {
+        boolean up = false;
+        for (int i = 1, s = 0; i < arr.length; i++) {
+            if (up) {
+                if (arr[i] < arr[i - 1]) {
+                    System.out.println("(" + s + ": " + (i - 1) + "), " + arr[i - 1]);
+                    up = false;
+                } else if (arr[i] > arr[i - 1]) {
+                    s = i;
+                }
+            } else {
+                if (arr[i] > arr[i - 1]) {
+                    up = true;
+                    s = i;
+                }
+            }
+        }
+    }
+}
